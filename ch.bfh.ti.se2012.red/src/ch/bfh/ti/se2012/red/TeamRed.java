bb363a029df799bfa882c2d1d23f5e782fc45aa0
loosh1 <loosh1@bfh.ch>
2012-12-18 12:23:31 +0100

    Start aktuallieseren

--- ch.bfh.ti.se2012.red/src/ch/bfh/ti/se2012/red/TeamRed.java
+++ ch.bfh.ti.se2012.red/src/ch/bfh/ti/se2012/red/TeamRed.java
@@ -1,5 +1,7 @@
 package ch.bfh.ti.se2012.red;
 
+import ch.bfh.ti.se2012.red.login.LoginWindow;
+
 import com.vaadin.Application;
 import com.vaadin.ui.*;
 
@@ -7,8 +9,9 @@ public class TeamRed extends Application {
 	@Override
 	public void init() {
 		Window mainWindow = new Window("TeamRed");
-		Label label = new Label("Hello Vaadin user");
-		mainWindow.addComponent(label);
+		mainWindow.getContent().setSizeFull();
+		LoginWindow login = new LoginWindow();
+		mainWindow.addComponent(login);
 		setMainWindow(mainWindow);
 	}
 
